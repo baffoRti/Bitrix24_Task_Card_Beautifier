c067586ceb4578237f879acec1898a73e14f7fe8
baffoRti <saha14373@gmail.com>
2026-08-07 17:18:52 +0300

    Reuse running browser for extension reload

--- update.bat
+++ update.bat
@@ -21,6 +21,12 @@ exit /b 0
 
 :open_extensions
 set /a browser_count=0
+call :add_running_browser "Google Chrome" "chrome" "chrome://extensions/"
+call :add_running_browser "Microsoft Edge" "msedge" "edge://extensions/"
+call :add_running_browser "Yandex Browser" "browser" "browser://extensions/"
+call :add_running_browser "Opera" "opera" "opera://extensions/"
+call :add_running_browser "Brave" "brave" "brave://extensions/"
+call :add_running_browser "Vivaldi" "vivaldi" "vivaldi://extensions/"
 call :add_browser "Google Chrome" "%ProgramFiles%\Google\Chrome\Application\chrome.exe" "chrome://extensions/"
 call :add_browser "Google Chrome" "%ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe" "chrome://extensions/"
 call :add_browser "Google Chrome" "%LocalAppData%\Google\Chrome\Application\chrome.exe" "chrome://extensions/"
@@ -45,8 +51,12 @@ for /L %%I in (1,1,!browser_count!) do echo %%I. !browser_name_%%I!
 set /p browser_choice=Browser number: 
 
 for /L %%I in (1,1,!browser_count!) do (
-  if "!browser_choice!"=="%%I" start "" /B "!browser_path_%%I!" --new-tab "!browser_page_%%I!"
+  if "!browser_choice!"=="%%I" start "" /B "!browser_path_%%I!" "!browser_page_%%I!"
 )
+exit /b 0
+
+:add_running_browser
+for /f "usebackq delims=" %%P in (`powershell -NoProfile -Command "$p = Get-Process -Name '%~2' -ErrorAction SilentlyContinue ^| Select-Object -First 1; if ($p) { [Console]::Write($p.Path) }"`) do call :add_browser "%~1 (running)" "%%P" "%~3"
 exit /b 0
 
 :add_browser
