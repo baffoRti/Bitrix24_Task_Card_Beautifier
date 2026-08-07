bccb3b0a4f4b6dbdb0fca7e49b64761445253304
baffoRti <saha14373@gmail.com>
2026-08-07 16:03:40 +0300

    Open extensions page after update

--- update.bat
+++ update.bat
@@ -5,7 +5,15 @@ cd /d "%~dp0"
 
 echo Updating Bitrix24 Task Card Beautifier...
 git pull --ff-only
+if errorlevel 1 (
+  echo.
+  echo Update failed. Fix the Git error and try again.
+  pause
+  exit /b 1
+)
 
 echo.
-echo Done. Open chrome://extensions/ and reload the extension.
+echo Done. Opening chrome://extensions/...
+start "" "chrome://extensions/"
+echo Reload the extension to apply the update.
 pause
